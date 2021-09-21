b8a60086603e80d1825ec6dbf8f238a565961a8a
umairiqbal78 <umairiqbal9889@gmail.com>
2021-09-21 22:34:15 +0500

    updating api integration problems fixes

--- lib/screens/home.dart/home.dart
+++ lib/screens/home.dart/home.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:stop_shop/screens/services/api.dart';
 
 import 'package:stop_shop/screens/services/auth.dart';
-import 'package:stop_shop/models/data.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -12,6 +12,19 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final AuthService _auth = AuthService();
+  List data = [{}];
+  @override
+  void initState() {
+    super.initState();
+    getDataList();
+  }
+
+  getDataList() async {
+    var _datafromApi = await getData();
+    setState(() {
+      data = _datafromApi;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +40,36 @@ class _HomeState extends State<Home> {
                 label: Text("Sign Out")),
           ],
         ),
-        body: Container());
+        body: Container(
+          padding: EdgeInsets.all(20.0),
+          child: data.isEmpty
+              ? Center(child: CircularProgressIndicator())
+              : ListView.separated(
+                  separatorBuilder: (BuildContext context, int index) =>
+                      Divider(color: Colors.grey[900]),
+                  itemCount: data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    var listdata = data[index];
+                    if (listdata.isEmpty) {
+                      return Center(child: CircularProgressIndicator());
+                    } else {
+                      return ListTile(
+                        title: Text(listdata.isEmpty ? '' : listdata['title']),
+                        subtitle:
+                            Text(listdata.isEmpty ? '' : listdata['category']),
+                        leading: SizedBox(
+                            height: 50,
+                            width: 70,
+                            child: Image.network(listdata.isEmpty
+                                ? ''
+                                : listdata['image'].toString())),
+                        trailing: Text(listdata.isEmpty
+                            ? ''
+                            : listdata['price'].toString()),
+                      );
+                    }
+                  },
+                ),
+        ));
   }
 }
